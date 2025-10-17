bf36c89e9014229e4753432add2cce2f746fcb9f
ANA JULLYA MIRANDA DOS SANTOS <anajullyamirandadossantos@gmail.com>
2025-10-17 16:44:52 -0300

    Add files via upload

---
+++ Atividade9/Atividade9/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] nomes = { "Ana", "Bruno", "Carla", "Daniel", "Eva" };
+
+
+Console.WriteLine("Digite um nome para pesquisar:");
+string nomeParaPesquisar = Console.ReadLine();
+
+
+bool nomeEncontrado = false;
+
+foreach (string nome in nomes)
+{
+    
+    if (nome.ToLower() == nomeParaPesquisar.ToLower())
+    {
+        nomeEncontrado = true;
+        break; 
+    }
+}
+
+if (nomeEncontrado)
+{
+    Console.WriteLine($"O nome {nomeParaPesquisar} foi encontrado .");
+}
+else
+{
+    Console.WriteLine($"O nome {nomeParaPesquisar} não está .");
+}
